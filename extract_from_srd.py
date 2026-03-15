7bb4abfba42a17f5078396ef9d0f1939e24cd117
TheMiles <miles@coders-of-the-caribbean.org>
2026-03-15 11:06:20 +0100

    remove columns and get linear text flow

--- extract_from_srd.py
+++ extract_from_srd.py
@@ -3,6 +3,52 @@
 import sys,os,argparse
 import subprocess
 
+def findColumnGap( page ):
+
+    for divider in range(55, 70):
+
+        s = set()
+
+        for l in page:
+
+            if len( l ) <= divider: continue
+
+            s.add( l[ divider ] )
+
+        if len( s ) == 1 and  ' ' in s:
+
+            return divider
+
+    return -1
+
+
+def split( page, divide_at ):
+
+
+    if divide_at < 0: return page,[]
+
+    first  = []
+    second = []
+
+    for l in page:
+
+        if len(l) <= divide_at:
+            
+            first.append( l )
+            continue
+
+        first.append(  l[ : divide_at ] )
+        second.append( l[ divide_at : ] )
+
+    return first, second
+
+
+def decolumnize( page ):
+
+    d   = findColumnGap( page )
+    f,s = split( page, d )
+
+    return f + s
 
 
 def parse_page( page, file ):
@@ -17,6 +63,7 @@ def parse_page( page, file ):
     )
 
     r = r.stdout.split('\n')
+    r = r[:-2]
     return r
 
 
@@ -24,12 +71,15 @@ def main():
 
     global args
 
-    print( F"Mein file ist {args.input[0]}")
+    pages = [ parse_page( x, args.input[0] ) for x in range( args.first, args.last + 1 ) ]
+    pages = [ decolumnize( p ) for p in pages ]
 
-    r = parse_page( args.first, args.input[0] )
-    for l in r:
-        print( l )
+    for p in pages:
 
+        for l in p:
+            print( l )
+
+            
 
 if __name__ == '__main__':
     try:
